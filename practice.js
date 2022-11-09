52b14e827f73a6c5f02565f6e47461b5fc7bd9e6
Abdul-Rehman-001 <114846612+Abdul-Rehman-001@users.noreply.github.com>
2022-11-10 02:13:20 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,30 @@
+const names=["Saad", "Saoud", "Jabbar", "Riyaz", "Irfan", "Ashfaq", "Aatif"]
+const result=names.sort((acc, cur)=>(acc < cur ?1:-1)/5+4)
+console.log(result)
+
+// const names2=["aamena", "naaz", "arbeena", "anisha"]
+// const result2=names2.map((item)=>item.toUpperCase())
+// console.log(result2)
+
+// const array=["aatif", "saad", "muzammil", "abdul", "riyaz", "saoud", "jabbar"]
+//  array.unshift("abcd")
+// console.log(array)
+// // array.shift()
+// console.log(array.length)
+
+// array.shift()
+// console.log(array)
+
+// array.push("pdac")
+// console.log(array)
+
+// array.pop()
+// console.log(array)
+// const abcd=array.slice(3,5)
+
+// console.log(abcd)
+
+// const numbers=[50, 66, 45, 79, 53, 88]
+// const result5= numbers.reduce((item)=> (acc+cur))
+// console.log(result)
+
